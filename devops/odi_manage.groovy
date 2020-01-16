162999f9d129a2682457496bdc85990da51c5844
olcayozz <olcayozz@gmail.com>
2020-01-16 20:04:25 +0300

    odi manage updated

--- devops/odi_manage.groovy
+++ devops/odi_manage.groovy
@@ -1,1 +1,7 @@
-println "Hello World From Groovy Script..."
+def test1(){
+    println "Hello World From Groovy Script...TEST1"
+}
+def test2(){
+    println "Hello World From Groovy Script...TEST2"
+}
+return this
