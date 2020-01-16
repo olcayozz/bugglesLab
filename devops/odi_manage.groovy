9b2daa60b3f82ee7bb3bc9a20917dffe2d2540c1
olcayozz <olcayozz@gmail.com>
2020-01-16 21:34:46 +0300

    no message

--- devops/odi_manage.groovy
+++ devops/odi_manage.groovy
@@ -1,15 +1,22 @@
-import java.util.logging.*
-import javax.swing.tree.*
-import oracle.odi.core.*;
-import oracle.odi.core.config.*
-import oracle.odi.core.persistence.transaction.*
-import oracle.odi.core.persistence.transaction.support.*
-import oracle.odi.core.security.*
-import oracle.odi.domain.runtime.loadplan.*
-import oracle.odi.domain.runtime.scenario.*
-import oracle.odi.domain.runtime.scenario.finder.*
-import oracle.odi.impexp.*
-import oracle.odi.impexp.support.*
+import java.util.logging.Logger
+import javax.swing.tree.DefaultMutableTreeNode
+import java.util.logging.Level
+import oracle.odi.core.OdiInstance;
+import oracle.odi.core.config.MasterRepositoryDbInfo
+import oracle.odi.core.config.OdiInstanceConfig
+import oracle.odi.core.config.PoolingAttributes
+import oracle.odi.core.config.WorkRepositoryDbInfo
+import oracle.odi.core.persistence.transaction.ITransactionStatus
+import oracle.odi.core.persistence.transaction.support.DefaultTransactionDefinition
+import oracle.odi.core.security.Authentication
+import oracle.odi.domain.runtime.loadplan.OdiLoadPlan
+import oracle.odi.domain.runtime.scenario.OdiScenario
+import oracle.odi.domain.runtime.scenario.OdiScenarioFolder
+import oracle.odi.domain.runtime.scenario.finder.IOdiScenarioFinder
+import oracle.odi.domain.runtime.scenario.finder.IOdiScenarioFolderFinder
+import oracle.odi.impexp.EncodingOptions
+import oracle.odi.impexp.support.ExportServiceImpl
+import oracle.odi.impexp.support.ImportServiceImpl
 
 
 driver 			= "oracle.jdbc.OracleDriver"
