91d20f13f65219d84f9d91f1cd4c9f497bec62ea
olcayozz <olcayozz@gmail.com>
2020-02-13 09:36:20 +0300

    ID TO ID2

--- devops/odi_manage.groovy
+++ devops/odi_manage.groovy
@@ -28,14 +28,6 @@ sourceWorkrep 	= "WORKREP"
 sourceOdiUser	= "XXXXXXXX"
 sourceOdiUserPwd = "XXXXXXXX"
 
-targetUrl 		= "jdbc:oracle:thin:@YOUR_SERVER_INFO"
-targetSchema 	= "DEV_ODI_REPO"
-targetSchemaPwd = "SXXXXXXXX"
-targetWorkrep 	= "WORKREP"
-targetOdiUser	= "XXXXXXXX"
-targetOdiUserPwd = "XXXXXXXX"
-
-exportPath = "C:\\Odi"
 
 def listObjects (odiInstance,odiClass,listOfObjects) {
 	odiObjects = odiInstance.getTransactionalEntityManager().getFinder(odiClass).findAll().sort{it.name}
@@ -50,18 +42,7 @@ def main(){
 	sourceAuth = sourceOdiInstance.getSecurityManager().createAuthentication(sourceOdiUser, sourceOdiUserPwd.toCharArray())
 	sourceOdiInstance.getSecurityManager().setCurrentThreadAuthentication(sourceAuth)
 	
-	println("Connected to Source! Yay!")
+	println("Connected to source")
 	
-	targetMasterInfo = new  MasterRepositoryDbInfo(targetUrl, driver, targetSchema, targetSchemaPwd.toCharArray(), new PoolingAttributes())
-	targetWorkInfo = new WorkRepositoryDbInfo(targetWorkrep, new PoolingAttributes())
-	
-	targetOdiInstance = OdiInstance.createInstance(new OdiInstanceConfig(targetMasterInfo, targetWorkInfo))
-	targetAuth = targetOdiInstance.getSecurityManager().createAuthentication(targetOdiUser, targetOdiUserPwd.toCharArray())
-	targetOdiInstance.getSecurityManager().setCurrentThreadAuthentication(targetAuth)
-	
-	println("Connected to Target! Yay!")
-}
-def test2(){
-    println "Hello World From Groovy Script...TEST2 "
 }
 return this
